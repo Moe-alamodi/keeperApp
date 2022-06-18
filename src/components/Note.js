746f72a1de26f45780602d6181a9a485c7404c99
Moe <mohammed@dnsnetworks.com>
2022-06-17 22:44:28 -0400

    Added new note section

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Note.css";
 const Note = (props) => {
+  // const [isMousedOver, setIsMousedOver] = useState(false);
+  // const onMouseOverHandler = () => {
+  //   setIsMousedOver(true);
+  // };
+  // const onMouseOutHandler = () => {
+  //   setIsMousedOver(false);
+  // };
+  const handleClick = (event) => {
+    props.onDelete(props.id);
+  };
   return (
-    <div className="note">
+    <div
+      className="note"
+      // onMouseOver={onMouseOverHandler}
+      // onMouseOut={onMouseOutHandler}
+      // style={{ transform: isMousedOver ? "scale(1.1)" : "scale(1)" }}
+    >
       <h1>{props.title}</h1>
       <p>{props.content}</p>
+      <button onClick={handleClick}>
+        <FontAwesomeIcon icon="fa-solid fa-trash" />
+      </button>
     </div>
   );
 };
